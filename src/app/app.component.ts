fcd6b6848ed0990bb6b5db4ca71442d929f18875
AlexeyLavrishin <alexeylavrishin@gmail.com>
2025-02-01 00:45:01 +0300

    feat:add base layout component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core'
-import { SvgIconComponent } from '@shared/components/svg-icon/svg-icon.component'
-import { NgOptimizedImage } from '@angular/common'
 import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SvgIconComponent, NgOptimizedImage, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
