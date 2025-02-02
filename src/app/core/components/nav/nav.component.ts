0ac26cbe6848ab19dcf7a8934d76fce9f600aba3
AlexeyLavrishin <alexeylavrishin@gmail.com>
2025-02-02 17:38:24 +0300

    feat: add nav component to layout header

--- src/app/core/components/nav/nav.component.ts
+++ src/app/core/components/nav/nav.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { SvgIconComponent } from '@shared/components/svg-icon/svg-icon.component'
+import ButtonComponent from '@shared/components/button/button.component'
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, ButtonComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
