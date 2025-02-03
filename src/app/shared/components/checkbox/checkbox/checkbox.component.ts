e6baf844daacc8a3aea6cf3a2b6846923474c275
AlexeyLavrishin <alexeylavrishin@gmail.com>
2025-02-04 02:43:37 +0300

    feat: custom checkbox

---
+++ src/app/shared/components/checkbox/checkbox/checkbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'input[appCheckbox]',
+  standalone: true,
+  imports: [],
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.scss',
+})
+export class CheckboxComponent {}
